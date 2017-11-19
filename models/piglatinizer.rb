2b0949a4b41aa8598dd57f5e78488e8a15ce3a8b
Mars Howard <marsbhoward@gmail.com>
2017-11-19 03:53:10 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,38 @@
+class PigLatinizer
+
+  def piglatinize(word)
+
+    non_pig_latin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on","until"]
+    vowels = ["a", "e", "i", "o", "u"]
+
+    if non_pig_latin_words.include?(word.downcase)
+      #case word
+      #when "an"
+        #word <<"way"
+      #when "i"
+        #word <<""
+      #else
+        word<<"way"
+      #end
+    elsif vowels.include? word[0]
+      word << "ay"
+    else
+      consonants = ""
+      while !vowels.include?(word[0])
+        consonants << word[0]
+        word = word.split("")[1..-1].join
+      end
+      word + consonants + 'ay'
+    end
+  end
+
+
+  def to_pig_latin(string)
+    string.split.collect{|word| piglatinize(word)}.join(" ")
+  end
+
+end
+
+p = PigLatinizer.new
+
+puts p.piglatinize("pork")
